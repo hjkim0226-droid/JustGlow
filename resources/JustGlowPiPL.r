d434590defa71955eb0cb44a3a4fd514a03dc490
김현중 <kimhyunjoong@gimhyeonjung-ui-MacBookPro.local>
2025-12-18 19:38:56 +0900

    fix: Update PiPL version to 1.2.0 to match code version

--- resources/JustGlowPiPL.r
+++ resources/JustGlowPiPL.r
@@ -53,7 +53,7 @@ resource 'PiPL' (16000) {
         },
 
         AE_Effect_Version {
-            0x00080001  /* 1.0.0 - PF_VERSION(1,0,0,PF_Stage_DEVELOP,1) */
+            0x00090001  /* 1.2.0 - PF_VERSION(1,2,0,PF_Stage_DEVELOP,1) */
         },
 
         AE_Effect_Info_Flags {
